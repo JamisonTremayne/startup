4823bc39a514f85d3f1fc175764db9ec9229810f
JamisonTremayne <jamisontremayne@gmail.com>
2026-02-09 21:54:47 -0700

    Added app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css'; -- I don't currently use bootstrap.
+import './app.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">App will display here</div>;
+}

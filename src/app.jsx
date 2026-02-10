3ab38072c86ef242971479697937274acf2f0aa4
JamisonTremayne <jamisontremayne@gmail.com>
2026-02-09 22:01:18 -0700

    Updated Header/Footer in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css'; -- I don't currently use bootstrap.
 import './app.css';
 
+
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body">
+        <header>
+            <img src="pixel_hoarder_title.png" alt="Pixel Hoarder Title" className="mx-auto" width="500"/>
+            <hr />
+        </header>
+
+        <footer>
+            <hr />
+            <p>Author: Jamison Tremayne</p>
+            <NavLink href="https://github.com/JamisonTremayne/startup">Github Link</NavLink>
+        </footer>
+    </div>
+  );
 }

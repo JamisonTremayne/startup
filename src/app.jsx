c5b7a5fc6c53dd3432b8d246a7133acd3338bb66
JamisonTremayne <jamisontremayne@gmail.com>
2026-02-09 22:06:02 -0700

    Setup basic Routes

--- src/app.jsx
+++ src/app.jsx
@@ -4,23 +4,37 @@ import './app.css';
 
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
-import { Game } from './game/game';
 import { Register } from './register/register';
+import { Game } from './game/game';
 import { Social } from './social/social';
 
 export default function App() {
   return (
-    <div className="body">
-        <header>
-            <img src="pixel_hoarder_title.png" alt="Pixel Hoarder Title" className="mx-auto" width="500"/>
-            <hr />
-        </header>
+    <BrowserRouter>
+        <div className="body">
+            <header>
+                <img src="pixel_hoarder_title.png" alt="Pixel Hoarder Title" className="mx-auto" width="500"/>
+                <hr />
+            </header>
 
-        <footer>
-            <hr />
-            <p>Author: Jamison Tremayne</p>
-            <NavLink href="https://github.com/JamisonTremayne/startup">Github Link</NavLink>
-        </footer>
-    </div>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/game' element={<Game />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/social' element={<Social />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+
+            <footer>
+                <hr />
+                <p>Author: Jamison Tremayne</p>
+                <NavLink href="https://github.com/JamisonTremayne/startup">Github Link</NavLink>
+            </footer>
+        </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main>404: Return to sender. Address unknown.</main>;
 }

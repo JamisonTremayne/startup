f028c10cf7ff62a69a900e4fab50dd1953c2e00f
JamisonTremayne <jamisontremayne@gmail.com>
2026-02-09 22:04:00 -0700

    Added starting functions for each page

---
+++ src/social/social.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Social() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Social displayed here</div>
+    </main>
+  );
+}

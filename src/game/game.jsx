1181a9f17313d1cc141209d90a965e652ababe55
JamisonTremayne <jamisontremayne@gmail.com>
2026-02-23 23:17:35 -0700

    Modularized pixel displays

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -49,6 +49,37 @@ export function Game(props) {
         const amount = 2 ** upgradeExpo;
         setUserData(prev => incrementPixel(prev, color, amount));
     }
+
+    function displayPixels(r_px, g_px, b_px) {
+        return (
+            <div className="pixels">
+                <div className="pixel-row">
+                    <div className="red-pixel">
+                        <svg width="10" height="10">
+                            <rect width="10" height="10" fill="rgb(255,80,80)"/>
+                        </svg>
+                    </div>
+                    <div className="red-pixels">{r_px}</div>
+                </div>
+                <div className="pixel-row">
+                    <div className="green-pixel">
+                        <svg width="10" height="10">
+                            <rect width="10" height="10" fill="rgb(80,255,80)"/>
+                        </svg>
+                    </div>
+                    <div className="green-pixels">{g_px}</div>
+                </div>
+                <div className="pixel-row">
+                    <div className="blue-pixel">
+                        <svg width="10" height="10">
+                            <rect width="10" height="10" fill="rgb(80,80,255)"/>
+                        </svg>
+                    </div>
+                    <div className="blue-pixels">{b_px}</div>
+                </div>
+            </div>
+        );
+    }
     
   return (
           <main>
@@ -62,11 +93,11 @@ export function Game(props) {
             <div id="random-quote">Random Quote: "{quote}" -{quoteAuthor} </div>
             <hr />
             <div id="pixel-display">
-                <div className="pixel-numbers">
-                    <div className="red-pixels">{formatNumber(pixels.red)}</div>
-                    <div className="green-pixels">{formatNumber(pixels.green)}</div>
-                    <div className="blue-pixels">{formatNumber(pixels.blue)}</div>
-                </div>
+                <div>{displayPixels(
+                    formatNumber(pixels.red),
+                    formatNumber(pixels.green),
+                    formatNumber(pixels.blue)
+                )}</div>
 
                 <div className="pixel-square">
                     <svg width="40" height="40">
